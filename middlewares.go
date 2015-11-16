982f31b6cede931276aa7e8b5d9d1dcea19f8e19
Adam Veldhousen <adam@veldhousen.ninja>
2015-11-16 01:20:52 -0600

    elapsed time middleware actually returns middleware

--- middlewares.go
+++ middlewares.go
@@ -29,19 +29,21 @@ func Count(next http.Handler) http.Handler {
 }
 
 // Elapsed time shows the total elapsed time of a request
-func ElapsedTime(next http.Handler, elapsedCb func(res http.ResponseWriter, req *http.Request, elapsed time.Duration)) http.Handler {
+func ElapsedTime(elapsedCb func(res http.ResponseWriter, req *http.Request, elapsed time.Duration)) Middleware {
 	if elapsedCb == nil {
 		elapsedCb = func(res http.ResponseWriter, req *http.Request, elapsed time.Duration) {
 			log.Printf("[DEBUG] %s took %s \n", req.URL.RawPath, elapsed)
 		}
 	}
 
-	return http.HandlerFunc(func(res http.ResponseWriter, req *http.Request) {
-		start := time.Now()
-		next.ServeHTTP(res, req)
-		elapsed := time.Since(start)
-		elapsedCb(res, req, elapsed)
-	})
+	return func(next http.Handler) http.Handler {
+		return http.HandlerFunc(func(res http.ResponseWriter, req *http.Request) {
+			start := time.Now()
+			next.ServeHTTP(res, req)
+			elapsed := time.Since(start)
+			elapsedCb(res, req, elapsed)
+		})
+	}
 }
 
 // StrictMethod forces a handler to only accept a set of defined HTTP Methods
